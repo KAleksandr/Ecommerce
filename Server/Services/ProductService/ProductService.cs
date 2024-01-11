9c88ae14e4dbbc96462f2b1a2db65ac5d053f0e2
KAleksandr <okuzmenkoa@gmail.com>
2024-01-11 21:51:12 +0200

    Display the Price in the ProductDetails Component

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -14,7 +14,7 @@ namespace Ecommerce.Server.Services.ProductService
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products.Include(p => p.Variants).ThenInclude(v => v.ProductType).FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
@@ -30,7 +30,7 @@ namespace Ecommerce.Server.Services.ProductService
 
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
             return response;
         }
@@ -39,7 +39,7 @@ namespace Ecommerce.Server.Services.ProductService
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.Where(p => p.Category.Url.ToLower() == categoryUrl.ToLower()).ToListAsync()
+                Data = await _context.Products.Where(p => p.Category.Url.ToLower() == categoryUrl.ToLower()).Include(p => p.Variants).ToListAsync()
             };
             return response;
         }
