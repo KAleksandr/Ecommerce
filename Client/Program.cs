26a4859d199a8312dbc85477b610ed6bf2a145a2
KAleksandr <okuzmenkoa@gmail.com>
2024-02-11 11:50:02 +0200

    doneversion

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,6 +5,7 @@ global using Ecommerce.Client.Services.ProductService;
 global using Ecommerce.Client.Services.CategoryService;
 global using Ecommerce.Client.Services.CartService;
 global using Ecommerce.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
@@ -25,7 +26,10 @@ namespace Ecommerce.Client
             builder.Services.AddScoped<IProductService, ProductService>();  
             builder.Services.AddScoped<ICategoryService, CategoryService>();  
             builder.Services.AddScoped<ICartService, CartService>();  
-            builder.Services.AddScoped<IAuthService, AuthService>();  
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
             await builder.Build().RunAsync();
         }
     }
