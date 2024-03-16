341d5fc00c1ecb3cc3590e8acdb0b31ad44d2d95
KAleksandr <okuzmenkoa@gmail.com>
2024-03-16 20:10:54 +0200

    Add More Properties to the Category Model

---
+++ Server/Migrations/20240316180510_CategoryFlags.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Delited",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Visible",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Delited", "Visible" },
+                values: new object[] { false, true });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Delited", "Visible" },
+                values: new object[] { false, true });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Delited", "Visible" },
+                values: new object[] { false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Delited",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Visible",
+                table: "Categories");
+        }
+    }
+}
