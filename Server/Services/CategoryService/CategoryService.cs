8d4104719e3728ba6138bbe48fb890e622583a04
KAleksandr <okuzmenkoa@gmail.com>
2024-03-19 21:07:39 +0200

    Implement all CRUD Operations for Categories on the

--- Server/Services/CategoryService/CategoryService.cs
+++ Server/Services/CategoryService/CategoryService.cs
@@ -10,16 +10,53 @@ namespace Ecommerce.Server.Services.CategoryService
         {
             _context = context;
         }
-        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+
+        public async Task<ServiceResponse<List<Category>>> AddCategory(Category category)
         {
-            var response = new ServiceResponse<List<Category>>()
-            {
-                Data = await _context.Categories.ToListAsync()
-            };
-            return response;
+            category.Editing = category.IsNew = false;
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return await GetAdminCategories();
         }
 
-        public async Task<ServiceResponse<Category>> GetCategoryAsync(int categoryId)
+        public async Task<ServiceResponse<List<Category>>> DeleteCategory(int id)
+        {
+          Category category =  await GetCategoryById(id);
+            if(category == null)
+            {
+                return new ServiceResponse<List<Category>> { Success = false, Message = "Sorry, but this category does not exist" };
+            }
+            category.Delited = true;
+            await _context.SaveChangesAsync();
+            return await GetAdminCategories();
+        }
+
+        private async Task<Category> GetCategoryById(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetAdminCategories()
+        {
+            var categories = await _context.Categories.Where(c => !c.Delited).ToListAsync();
+
+            return new ServiceResponse<List<Category>>()
+            {
+                Data = categories
+            };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategoriesAsync()
+        {
+            var categories = await _context.Categories.Where(c => !c.Delited && c.Visible).ToListAsync();
+            
+            return new ServiceResponse<List<Category>>()
+            {
+                Data = categories
+            };
+        }
+
+        public async Task<ServiceResponse<Category>> GetCategory(int categoryId)
         {
             var response = new ServiceResponse<Category>();
             var category = await _context.Categories.FindAsync(categoryId);
@@ -31,6 +68,20 @@ namespace Ecommerce.Server.Services.CategoryService
             else { response.Data = category; }
 
             return response;
+        }
+
+        public async Task<ServiceResponse<List<Category>>> UpdateCategory(Category category)
+        {
+            Category dbCategory = await GetCategoryById(category.Id);
+            if(dbCategory == null)
+            {
+                return new ServiceResponse<List<Category>> { Success = false, Message = "Sorry, but this category does not exist" };
+            }
+            dbCategory.Name = category.Name;
+            dbCategory.Url = category.Url;
+            dbCategory.Visible = category.Visible;
+            await _context.SaveChangesAsync();
+            return await GetAdminCategories();
         }
     }
 }
--- Server/Services/CategoryService/ICategoryService.cs
+++ Server/Services/CategoryService/ICategoryService.cs
@@ -3,6 +3,10 @@
     public interface ICategoryService
     {
         Task<ServiceResponse<List<Category>>> GetCategoriesAsync();
-        Task<ServiceResponse<Category>> GetCategoryAsync(int categoryId);
+        Task<ServiceResponse<Category>> GetCategory(int categoryId);
+        Task<ServiceResponse<List<Category>>> GetAdminCategories();
+        Task<ServiceResponse<List<Category>>> AddCategory(Category category);
+        Task<ServiceResponse<List<Category>>> UpdateCategory(Category category);
+        Task<ServiceResponse<List<Category>>> DeleteCategory(int id);
     }
 }
